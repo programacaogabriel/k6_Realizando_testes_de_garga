9f8446654dabdb624dc92dc43dc8c92bd6ec9b6e
programacaogabriel <programacaogabriel@gmail.com>
2023-01-31 14:41:35 -0300

    :test_tube: load test - carga constante
    Algumas solicitações falharam, feito com 150 vus e duração de 20 minutos

---
+++ Scenarios/loadTest/loadTest_150vus_20m/loadTest.js
@@ -0,0 +1,25 @@
+
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+
+//Carga Constante
+export const options = {
+    vus: 150,
+    duration: '20m'
+}
+
+
+export default function () {
+
+    http.get('https://netshoes.com.br');
+
+    sleep(1);
+
+}
+export function handleSummary(data) {
+    return {
+        "summaryCsv.html": htmlReport(data),
+    };
+}
