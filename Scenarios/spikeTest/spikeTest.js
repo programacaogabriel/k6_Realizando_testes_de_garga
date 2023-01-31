a60a095170866426c5daba3f931ff6919e865fa6
programacaogabriel <programacaogabriel@gmail.com>
2023-01-31 17:15:02 -0300

    :memo: stress e spike test
    adicionado as pastas com arquivos para iniciar os testes

---
+++ Scenarios/spikeTest/spikeTest.js
@@ -0,0 +1,33 @@
+
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+
+export const options = {
+    stages: [
+
+        { duration: '10s', target: 100 },
+        { duration: '1m', target: 100 },
+        { duration: '10s', target: 1400 },
+        { duration: '3m', target: 1400 },
+        { duration: '10s', target: 100 },
+        { duration: '3m', target: 100 },
+        { duration: '10s', target: 0 },
+
+    ]
+}
+
+
+export default function () {
+
+    http.get('https://netshoes.com.br');
+
+    sleep(1);
+
+}
+export function handleSummary(data) {
+    return {
+        "summary.html": htmlReport(data),
+    };
+}
