1cd9dc7b23eebec4fe86b5e68df29c97bb1f658f
programacaogabriel <programacaogabriel@gmail.com>
2023-02-07 23:42:27 -0300

    :white_check_mark: load Test
    realização de load teste com 50vus em 10 minutos

---
+++ Scenarios/loadTest/loadTest_50vus_10m/loadTest.js
@@ -0,0 +1,25 @@
+
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+
+//Carga Constante
+export const options = {
+    vus: 50,
+    duration: '10m'
+}
+
+
+export default function () {
+
+    http.get('https://netshoes.com.br');
+
+    sleep(1);
+
+}
+export function handleSummary(data) {
+    return {
+        "summaryCsv.html": htmlReport(data),
+    };
+}
